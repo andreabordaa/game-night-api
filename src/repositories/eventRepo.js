28da93addc8b8edc1c2ee07d9ed3b500fd32fe13
Andrea Borda <aborda2@uncc.edu>
2025-11-21 22:15:52 -0500

    complete event crud

--- src/repositories/eventRepo.js
+++ src/repositories/eventRepo.js
@@ -1,5 +1,70 @@
 import prisma from '../config/db.js';
 
+// GET /events/host
 export async function findCurrentUserEvents(id) {
   return await prisma.event.findMany({ where: { hostId: id } });
+}
+
+// GET /events/:id
+export async function findById(id) {
+  return await prisma.event.findUnique({ where: { id } });
+}
+
+// POST /events/
+export async function create(event) {
+  const newEvent = await prisma.event.create({
+    data: event,
+  });
+  return newEvent;
+}
+
+// PUT /events/:id
+export async function update(id, updates) {
+  try {
+    const updatedEvent = await prisma.event.update({
+      where: { id },
+      data: updates,
+    });
+    return updatedEvent;
+  } catch (error) {
+    if (error.code === 'P2025') return null;
+    throw error;
+  }
+}
+
+// DELETE /events/:id
+export async function remove(id) {
+  try {
+    const deletedEvent = await prisma.event.delete({
+      where: { id },
+    });
+    return deletedEvent;
+  } catch (error) {
+    if (error.code === 'P2025') return null;
+    throw error;
+  }
+}
+
+// POST /events/:id/join
+export async function findEvent(id) {
+  const joinedEvent = await prisma.event.findUnique({
+    where: { id },
+    include: { attendees: true },
+  });
+
+  return joinedEvent;
+}
+
+export async function join(eventId, userId) {
+  const joinedUser = await prisma.event.update({
+    where: { id: eventId },
+    data: {
+      attendees: {
+        connect: { id: userId },
+      },
+    },
+    include: { attendees: true },
+  });
+
+  return joinedUser;
 }
--- src/services/eventService.js
+++ src/services/eventService.js
@@ -1,5 +1,66 @@
 import * as eventRepo from '../repositories/eventRepo.js';
 
+// GET /events/host
 export async function getCurrentUserEvents(id) {
   return await eventRepo.findCurrentUserEvents(id);
+}
+
+// GET /events/:id
+export async function getEventById(id) {
+  let result = await eventRepo.findById(id);
+  if (result) return result;
+  else {
+    const error = new Error(`Cannot find event with id ${id}`);
+    error.status = 404;
+    throw error;
+  }
+}
+
+// POST /events/
+export async function createEvent(data) {
+  return await eventRepo.create(data);
+}
+
+// PUT /events/:id
+export async function updateEvent(id, data) {
+  const updatedEvent = await eventRepo.update(id, data);
+  if (updatedEvent) return updatedEvent;
+  else {
+    const error = new Error(`Cannot find event with id ${id}`);
+    error.status = 404;
+    throw error;
+  }
+}
+
+// DELETE /events/:id
+export async function deleteEvent(id) {
+  const result = await eventRepo.remove(id);
+  if (result) return;
+  else {
+    const error = new Error(`Cannot find event with id ${id}`);
+    error.status = 404;
+    throw error;
+  }
+}
+
+// POST /events/:id/join
+export async function joinEvent(eventId, userId) {
+  const event = await eventRepo.findEvent(eventId);
+
+  if (!event) {
+    const error = new Error(`Cannot find event with id ${eventId}`);
+    error.status = 404;
+    throw error;
+  }
+
+  const alreadyJoined = event.attendees.some(
+    (attendee) => attendee.id === userId,
+  );
+  if (alreadyJoined) {
+    const error = new Error('Already joined event');
+    error.status = 409;
+    throw error;
+  }
+
+  return await eventRepo.join(eventId, userId);
 }
